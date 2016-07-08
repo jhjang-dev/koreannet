70352f948a90ff8a736af296260da60b67f7739e
Jungho Jang <jhjjang@gmail.com>
2016-07-08 10:52:49 +0900

    add example file

---
+++ example.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"api/koreannet"
+	"flag"
+	"fmt"
+	"os"
+	"runtime"
+	"strings"
+	"sync"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var wg sync.WaitGroup
+
+func main() {
+	mode := flag.String("mode", "single or server", "형식")
+
+	flag.Parse()
+
+	if flag.NFlag() == 0 {
+		flag.Usage()
+		return
+	}
+
+	if *mode == "server" {
+		r := gin.Default()
+		r.GET("/search", func(c *gin.Context) {
+
+			code := c.Query("barcode")
+			id := c.Query("id")
+			pw := c.Query("pw")
+
+			seq := 1
+
+			var result bool
+			wg.Add(1)
+			go func(wg sync.WaitGroup, seq int, code string, id string, pw string) {
+				result = koreannet.Parse(&wg, seq, code, id, pw)
+			}(wg, seq, code, id, pw)
+
+			c.JSON(200, gin.H{
+				"barcode": code,
+				"id":      id,
+				"pw":      pw,
+				"result":  result,
+			})
+		})
+		wg.Wait()
+		r.Run(":9005") // listen and server on 0.0.0.0:8080
+	} else {
+
+		runtime.GOMAXPROCS(runtime.NumCPU())
+
+		if len(os.Args) < 3 {
+			fmt.Println("Usage: file -mode=single barcode^^id^^password")
+			return
+		}
+
+		barcodes := os.Args[2:]
+
+		seq := 1
+		for _, dt := range barcodes {
+
+			result := strings.Split(dt, "^^")
+			arr_len := len(result)
+
+			if arr_len < 3 {
+
+				fmt.Println("Format is incorrect")
+				continue
+			}
+
+			code := result[0]
+			id := result[1]
+			pw := result[2]
+
+			wg.Add(1)
+			go koreannet.Parse(&wg, seq, code, id, pw)
+
+			if seq%10 == 0 {
+				fmt.Println("wait:", seq)
+				wg.Wait()
+			}
+
+			seq += 1
+		}
+		wg.Wait()
+
+	}
+
+}
